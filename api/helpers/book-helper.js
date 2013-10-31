6452b9e159d37fc8d0b2d51fe9ae868bc21a6d46
luxinjian <luxinjian321@gmail.com>
2013-10-31 14:27:01 +0800

    Finish book upload

--- api/helpers/book-helper.js
+++ api/helpers/book-helper.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 
 exports.getChapter = function(book_name, cpt_name, callback) {
-  book_name = book_name.trim();
-  cpt_name = cpt_name.trim();
   var path = sails.config.book_path + book_name + '/' + cpt_name + '.txt';
   fs.readFile(path, function(err, data) {
     if (err) {
@@ -11,4 +9,34 @@ exports.getChapter = function(book_name, cpt_name, callback) {
       callback(null, data.toString());
     }
   });
-}
+};
+
+exports.saveBook = function(name, path, callback) {
+  // Read data from upload file
+  fs.readFile(path, function(err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      var current_book_path = sails.config.book_path + name;
+      // Make book folder
+      fs.mkdir(current_book_path, function(err) {
+        if (err) {
+          callback(err);
+        } else {
+          // Save book as one .txt file with book name as file name
+          var file_path = current_book_path + '/' + name + '.txt';
+          fs.open(file_path, 'wx', function(err, fd) {
+            if (err) {
+              callback(err);
+            } else {
+              // Write to file
+              fs.writeFile(file_path, data, function(err) {
+                err ? callback(err) : callback(null);
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+};
